9ffd0e8ff6d134f1bdf44e751727d852527c2469
Abhinav Agarwal <119868907+Abhinavagarwa@users.noreply.github.com>
2023-12-16 08:07:15 +0000

    first

---
+++ problemset.js
@@ -0,0 +1,20 @@
+//1st question
+let a="Abhinav";
+let n=7;
+console.log(a+n);
+//2nd question
+console.log(typeof a);
+//3rd question
+const a1={
+    nae:'Abhinav',
+    rollno:7,
+    principal:"venu gopal iyer"
+      
+}
+console.log(a1);
+//adding a key
+a1['friend']="Shaswat"
+console.log(a1);
+//udating a key 
+a1['nae']="Abhinav07";
+console.log(a1);
